b7f113e73e83943b366b16eaf650942ddffe89d0
Matteo Gregoricchio <matteo.gregoricchio@libero.it>
2021-04-19 22:34:13 +0200

    add MongoDB logger provider - first commit

---
+++ ElmahCore.MongoDB/MongoDBErrorLog.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace ElmahCore.MongoDB
+{
+    public sealed class MongoDBErrorLog : ErrorLog
+    {
+        private readonly IMongoCollection<MongoDBLogEntry> _collection;
+        /// <summary>
+        ///     Gets the name of this error log implementation.
+        /// </summary>
+        public override string Name => "MongoDB Error Log";
+
+        /// <summary>
+        ///     Gets the connection string used by the log to connect to the database.
+        /// </summary>
+        // ReSharper disable once MemberCanBeProtected.Global
+        public string ConnectionString { get; }
+        public MongoDBErrorLog(IMongoClient client, IOptions<ElmahOptions> options) : this(client, options.Value.ConnectionString, options.Value.DatabaseName, options.Value.CollectionName)
+        {
+        }
+        public MongoDBErrorLog(IMongoClient client, string connectionString, string databaseName, string collectionName = "elmahcore_collection")
+        {
+            //var x = options.Value.ConnectionString.Split('|');
+            //_factory = factory;
+            //var dbname = x[0];
+            var db = client.GetDatabase(databaseName);
+            var collectionname = "elmah_collection";
+            //_dbname = dbname;
+            //var data = new Func<IMongoCollection<MongoLogEntry>>(() => db.GetCollection<MongoLogEntry>(collectionname));
+
+            _collection = CreateCollectionIfNotExistent(db, collectionname);
+
+        }
+
+        private IMongoCollection<MongoDBLogEntry> CreateCollectionIfNotExistent(IMongoDatabase db, string collectionname)
+        {
+            var checkCollection = db.ListCollectionNames(new ListCollectionNamesOptions()
+            {
+                Filter = Builders<BsonDocument>.Filter.Eq("name", collectionname)
+            });
+            if (!checkCollection.Any())
+            {
+                db.CreateCollection(collectionname);
+            }
+            return db.GetCollection<MongoDBLogEntry>(collectionname);
+        }
+
+        public override ErrorLogEntry GetError(string id)
+        {
+            var errorLog = _collection.Find(a => a.ErrorId == id).FirstOrDefault();
+            return new ErrorLogEntry(this, id, ErrorXml.DecodeString(errorLog.XmlError));
+            //throw new NotImplementedException();
+        }
+
+        public override int GetErrors(int errorIndex, int pageSize, ICollection<ErrorLogEntry> errorEntryList)
+        {
+            var errorLog = _collection.Find(a => true).Skip(errorIndex).Limit(pageSize).ToList();
+            errorLog.ForEach(a => errorEntryList.Add(
+              new ErrorLogEntry(this, a.ErrorId, ErrorXml.DecodeString(a.XmlError))));
+            return errorLog.Count;
+            throw new NotImplementedException();
+        }
+
+        public override string Log(Error error)
+        {
+            var id = Guid.NewGuid();
+            Log(id, error);
+            return id.ToString();
+        }
+
+        public override void Log(Guid id, Error error)
+        {
+
+            var xmlerror = ErrorXml.EncodeString(error);
+            _collection.InsertOne(new MongoDBLogEntry(id.ToString(), xmlerror, error));
+            //throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ ElmahCore.MongoDB/MongoDBLogEntry.cs
@@ -0,0 +1,77 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElmahCore.MongoDB
+{
+    [BsonIgnoreExtraElements]
+    internal sealed class MongoDBLogEntry
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string ErrorId { get; set; }
+        [BsonIgnore]
+        public Error Error { get; set; }
+        public string XmlError { get; set; }
+        // mapping
+        public Dictionary<string, string[]> QueryString { get; set; }
+        public Dictionary<string, string[]> ServerVariables { get; set; }
+        public string WebHostHtmlMessage { get; set; }
+        public int StatusCode { get; set; }
+        public DateTime Time { get; set; }
+        public string User { get; set; }
+        public string Detail { get; set; }
+        public string Message { get; set; }
+        public string Source { get; set; }
+        public string Body { get; set; }
+        public string Type { get; set; }
+        public string HostName { get; set; }
+        public string ApplicationName { get; set; }
+        public Exception Exception { get; set; }
+        public List<ElmahLogParamEntry> Params { get; set; }
+        public List<ElmahLogMessageEntry> MessageLog { get; set; }
+        public Dictionary<string, string[]> Form { get; set; }
+        public Dictionary<string, string[]> Cookies { get; set; }
+
+        //[BsonElement("internalid")]
+        //public string Id { get; set; }
+        public MongoDBLogEntry(string id, string xml, Error error) //(ErrorLog log, string id, Error error) : base(log, id, error)
+        {
+            Id = ObjectId.GenerateNewId();
+            ErrorId = id;
+            XmlError = xml;
+            FromErrorToMongo(error);
+        }
+
+        public void FromErrorToMongo(Error error)
+        {
+            //https://stackoverflow.com/questions/7003740/how-to-convert-namevaluecollection-to-json-string
+            var d = error.QueryString.AllKeys.ToDictionary(k => k, k => error.QueryString.GetValues(k));
+            QueryString = d;
+            var e = error.ServerVariables.AllKeys.ToDictionary(k => k, k => error.ServerVariables.GetValues(k));
+            ServerVariables = e;
+            WebHostHtmlMessage = error.WebHostHtmlMessage;
+            StatusCode = error.StatusCode;
+            Time = error.Time;
+            User = error.User;
+            Detail = error.Detail;
+            Message = error.Message;
+            Source = error.Source;
+            Body = error.Body;
+            Type = error.Type;
+            HostName = error.HostName;
+            ApplicationName = error.ApplicationName;
+            Exception = error.Exception;
+            Params = error.Params;
+            MessageLog = error.MessageLog;
+            var g = error.Form.AllKeys.ToDictionary(k => k, k => error.Form.GetValues(k));
+            var h = error.Cookies.AllKeys.ToDictionary(k => k, k => error.Cookies.GetValues(k));
+            Form = g;
+            Cookies = h;
+
+            //throw new NotImplementedException();      //throw new NotImplementedException();
+        }
+    }
+}
